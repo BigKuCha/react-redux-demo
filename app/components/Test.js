bfc2686abe16a5ce0ecf268211990381653856d2
BigKuCha <nevermore1989@vip.qq.com>
2017-05-14 01:38:30 +0800

    click TBD

--- app/components/Test.js
+++ app/components/Test.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-const Test = () => {
+let Test = ({name}) => {
     return <div>
-        <h1>Hello React-Redux</h1>
+        <h1 onClick={() => onHelloClick()}>{name}</h1>
     </div>
 };
+
+function mapStateToProps(state) {
+    return {
+        name: state.helloText,
+        onHelloClick: () => {
+            alert('Yes')
+        }
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onHelloClick: () => {
+            alert('Yes');
+        }
+    }
+}
+Test = connect(mapStateToProps, mapDispatchToProps)(Test);
 
 export default Test;
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,7 +2,12 @@ import {combineReducers} from 'redux'
 import todos from './todos'
 import visibilityFilter from './visibilityFilter'
 
+const helloText = (state = 'kkkkk', action) => {
+    return 'Hello React-Redux';
+};
+
 const todoApp = combineReducers({
+    helloText,
     todos,
     visibilityFilter
 });
